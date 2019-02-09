7fbe4a5dc4961a749226c17dc81214581fb0e187
corey <coreycradduck@gmail.com>
2019-02-09 17:26:11 +0100

    Added color changing to objects

--- Assets/Scripts/ControlObject.cs
+++ Assets/Scripts/ControlObject.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyObject : MonoBehaviour
+public class ControlObject : MonoBehaviour
 {
+    public Material sphereMat;
+    public Material noGravityMat;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,20 +16,23 @@ public class DestroyObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Destroy game object
         if (Input.GetMouseButtonDown(1))
         {
             Destroy(gameObject);
         }
 
-        // Turn off gravity with spacebar
+        // Turn off gravity and change color with spacebar
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (gameObject.GetComponent<Rigidbody2D>().gravityScale > 0)
             {
+                gameObject.GetComponent<Renderer>().material = noGravityMat;
                 gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
             }
             else
             {
+                gameObject.GetComponent<Renderer>().material = sphereMat;
                 gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
             }
         }
